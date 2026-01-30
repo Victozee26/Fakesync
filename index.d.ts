dd6287e50ea2b94c61139fb844720a0f60f2bb76
victor LP <victorelisha26@gmail.com>
2026-01-30 08:37:51 +0100

    Improve TypeScript types: use module augmentation with direct Parameters/ReturnType mapping

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,22 @@
-export interface FakeSyncOptions {
-  minDelay?: number;
-  maxDelay?: number;
-  failRate?: number;
+declare module 'fakesync' {
+  export interface FakeSyncOptions {
+    minDelay?: number;
+    maxDelay?: number;
+    failRate?: number;
+  }
+
+  export interface FakeSync {
+    defaults: FakeSyncOptions;
+    registerDefaults(options: FakeSyncOptions): void;
+    register<T extends Record<string, (...args: unknown[]) => unknown>>(
+      funcs: T,
+      options?: FakeSyncOptions
+    ): {
+      [K in keyof T]: (...args: Parameters<T[K]>) => Promise<Awaited<ReturnType<T[K]>>>;
+    };
+  }
+
+  const fakesync: FakeSync;
+  export default fakesync;
+  export { fakesync };
 }
-
-export interface FakeSyncFunctions {
-  [key: string]: (...args: any[]) => any;
-}
-
-export type Promisified<F> = F extends (...args: infer A) => infer R ? (...args: A) => Promise<Awaited<R>> : never;
-
-export type PromisifiedMap<T extends Record<string, (...args: any[]) => any>> = {
-  [K in keyof T]: Promisified<T[K]>;
-};
-
-export interface FakeSync {
-  defaults: FakeSyncOptions;
-  registerDefaults(options: FakeSyncOptions): void;
-  register<T extends Record<string, (...args: any[]) => any>>(
-    funcs: T,
-    options?: FakeSyncOptions
-  ): PromisifiedMap<T>;
-}
-
-declare const fakesync: FakeSync;
-
-export default fakesync;
-export { fakesync };
